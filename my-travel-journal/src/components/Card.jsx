ce231dae2fc5411a4571f1aef88e4fb7d668d75e
cameronpool <cameronpool2019@gmail.com>
2024-08-19 16:18:47 -0500

    all cards complete (need to add media quieries)

--- my-travel-journal/src/components/Card.jsx
+++ my-travel-journal/src/components/Card.jsx
@@ -1,25 +1,26 @@
-import '../App.css'
-import pin from '../assets/pinpoint.png'
+import "../App.css";
+import pin from "../assets/pinpoint.png";
 
 export default function Card(props) {
-
-    return (
-        <div className='card'>
-            <img src={props.imageUrl} alt={`Picture of ${props.title}`} />
-            <img src={pin} alt="map pinpoint" />
-            <h4>{props.location}</h4>
-            <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
-            <h2>{props.title}</h2>
-            <p>{`${props.startDate} - ${props.endDate}`}</p>
-            <p>{props.description}</p>
+  return (
+    <div className="card-container">
+      <img
+        className="card-img"
+        src={props.imageUrl}
+        alt={`Picture of ${props.title}`}
+      />
+      <div className="card-stats">
+        <div className="card-location">
+          <img className="card-pin" src={pin} alt="map pinpoint" />
+          <h4>{props.location}</h4>
+          <p>
+            <a href={props.googleMapsUrl}>View on Google Maps</a>
+          </p>
         </div>
-    )
+        <h2 className="card-title" >{props.title}</h2>
+        <p className="card-dates">{`${props.startDate} - ${props.endDate}`}</p>
+        <p className="card-description">{props.description}</p>
+      </div>
+    </div>
+  );
 }
-
-// title: "Mount Fuji",
-//         location: "Japan",
-//         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-//         startDate: "12 Jan, 2021",
-//         endDate: "24 Jan, 2021",
-//         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-//         imageUrl: "htt
